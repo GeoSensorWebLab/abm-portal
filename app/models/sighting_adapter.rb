1d1959a72bf7dd98ac6137bbbca0067648e8c377
James Badger <james@jamesbadger.ca>
2015-06-01 10:00:31 -0600

    Add adapter class to get data from server
    Parsing will be a separate class.

---
+++ app/models/sighting_adapter.rb
@@ -0,0 +1,36 @@
+class SightingAdapter
+  class << self
+    HOST = ENV['API_URL']
+    PATH = '/biomap/sightings'
+
+    def all
+      conn = Faraday.new(url: HOST)
+      response = conn.get do |req|
+        req.url PATH
+        req.params['sighting_id'] = '*'
+        req.params['user_id'] = '*'
+      end
+      response.body
+    end
+
+    def find_by_sighting_id(sighting_id)
+      conn = Faraday.new(url: HOST)
+      response = conn.get do |req|
+        req.url PATH
+        req.params['sighting_id'] = sighting_id
+        req.params['user_id'] = '*'
+      end
+      response.body
+    end
+
+    def find_by_user_id(user_id)
+      conn = Faraday.new(url: HOST)
+      response = conn.get do |req|
+        req.url PATH
+        req.params['sighting_id'] = '*'
+        req.params['user_id'] = user_id
+      end
+      response.body
+    end
+  end
+end
