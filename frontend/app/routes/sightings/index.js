bb76948a52f4b12033985e52914d3836d5cbf345
James Badger <james@jamesbadger.ca>
2015-06-02 15:15:36 -0600

    Fix routing and model loading

--- frontend/app/routes/sightings/index.js
+++ frontend/app/routes/sightings/index.js
@@ -4,10 +4,6 @@ import Sighting from '../../models/sighting';
 export default Ember.Route.extend({
   model: function() {
     var store = this.store;
-    store.pushPayload('sighting', {
-      sightings: window.sightings
-    });
-
     return store.all('sighting');
   }
 });
--- frontend/app/routes/sightings/show.js
+++ frontend/app/routes/sightings/show.js
@@ -1,13 +1,7 @@
 import Ember from 'ember';
-import Sighting from '../../models/sighting';
 
 export default Ember.Route.extend({
-  model: function(params, transition) {
-    var store = this.store;
-    store.pushPayload('sighting', {
-      sightings: window.sightings
-    });
-
-    return store.find('sighting', params.sighting_id);
+  model: function(params) {
+    return this.store.getById('sighting', params.sighting_id);
   }
 });
