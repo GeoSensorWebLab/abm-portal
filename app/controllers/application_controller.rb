0402fadc5cce6d79e165b3b31e33c474299ecd04
James Badger <james@jamesbadger.ca>
2015-06-03 14:48:27 -0600

    Add Vary header for multiple representations

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,14 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_filter :vary_by_accept
+
+  private
+
+  # This adds a Vary for responses. This is useful when returning a URL that
+  # has multiple representations depending on the Accepts header sent by the
+  # client.
+  def vary_by_accept
+    response.headers['Vary'] = 'Accept'
+  end
 end
