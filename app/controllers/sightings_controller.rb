ed2b720928bafb82f6c6706377672f724fe2f05f
James Badger <james@jamesbadger.ca>
2015-06-03 12:57:27 -0600

    Add json serialization for sightings

--- app/controllers/sightings_controller.rb
+++ app/controllers/sightings_controller.rb
@@ -1,10 +1,17 @@
 class SightingsController < ApplicationController
   def index
     @sightings = Sighting.all
+    respond_to do |format|
+      format.html
+      format.json { render json: @sightings }
+    end
   end
 
   def show
-    @sightings = Sighting.all
-    render 'index'
+    @sighting = Sighting.find_by_id(params['id'])
+    respond_to do |format|
+      format.html { render 'index' }
+      format.json { render json: @sighting }
+    end
   end
 end
