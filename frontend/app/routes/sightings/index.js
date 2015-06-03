3353e7cb1eef2d71e44ce0a3b3194069dd919e87
James Badger <james@jamesbadger.ca>
2015-06-03 13:17:45 -0600

    Load sightings from server in Ember

--- frontend/app/routes/sightings/index.js
+++ frontend/app/routes/sightings/index.js
@@ -1,9 +1,7 @@
 import Ember from 'ember';
-import Sighting from '../../models/sighting';
 
 export default Ember.Route.extend({
   model: function() {
-    var store = this.store;
-    return store.all('sighting');
+    return this.store.findAll('sighting');
   }
 });
