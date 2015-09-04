f9a3e0f5360ff929a9a4b21924112716a8510636
James Badger <james@jamesbadger.ca>
2015-09-04 15:03:44 -0600

    Improve ID filter on table/map select

--- frontend/app/components/sighting-table.js
+++ frontend/app/components/sighting-table.js
@@ -18,8 +18,25 @@ export default Ember.Component.extend({
     var table = this.$("table").DataTable();
     this.set('table', table);
 
+    // Filter the table by the ID attribute when changes are made to the
+    // input field for the ID column.
+    $("input[name=id-filter]").on('keyup change', function() {
+      var column = table.column(":contains(ID)");
+
+      if (this.value === "") {
+        column.search("").draw();
+      } else if (column.search() !== this.value) {
+        column.search(`^${this.value}$`, true, false).draw();
+      }
+    });
+
+    // When a 'select' event external to this component is triggered, then
+    // filter the table by that model's ID attribute.
     this.get('events').on('select', function(model) {
-      table.search(model.get('id')).draw();
+      var column = table.column(":contains(ID)");
+      var id = model.get('id');
+      $("input[name=id-filter]").val(id);
+      column.search(`^${id}$`, true, false).draw();
     });
   },
 
