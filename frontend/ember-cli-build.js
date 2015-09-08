eec7c3d1d388e8d175c1456f64883d0f70dcd05f
James Badger <james@jamesbadger.ca>
2015-09-08 15:03:31 -0600

    Replace Brocfile with ember-cli-build.js

---
+++ frontend/ember-cli-build.js
@@ -0,0 +1,39 @@
+var EmberApp = require('ember-cli/lib/broccoli/ember-app');
+
+module.exports = function(defaults) {
+    var app = new EmberApp(defaults, {
+        // Any other options
+    });
+
+    // Use `app.import` to add additional libraries to the generated
+    // output files.
+    //
+    // If you need to use different assets in different
+    // environments, specify an object as the first parameter. That
+    // object's keys should be the environment name and the values
+    // should be the asset to use in that environment.
+    //
+    // If the library that you are including contains AMD or ES6
+    // modules that you would like to import into your application
+    // please specify an object with the list of modules as keys
+    // along with the exports of each module as its value.
+    app.import('bower_components/leaflet/dist/leaflet.js');
+    app.import('bower_components/leaflet/dist/leaflet.css');
+    app.import('bower_components/leaflet.markercluster/dist/leaflet.markercluster.js');
+    app.import('bower_components/leaflet.markercluster/dist/MarkerCluster.css');
+
+    app.import('bower_components/proj4/dist/proj4.js');
+    app.import('bower_components/Proj4Leaflet/src/proj4leaflet.js');
+
+    app.import('bower_components/polarmap/dist/polarmap.js');
+    app.import('bower_components/polarmap/css/polarmap.css');
+
+    app.import('bower_components/DataTables/media/css/jquery.dataTables.css');
+    app.import('bower_components/DataTables/media/js/jquery.dataTables.js');
+
+    app.import("bower_components/momentjs/moment.js");
+
+    app.import("bower_components/typeahead.js/dist/typeahead.jquery.js");
+
+    return app.toTree();
+};
