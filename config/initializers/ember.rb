dfa20d2a084be0f0cf302a31420020b0294880f8
James Badger <james@jamesbadger.ca>
2015-09-08 14:51:58 -0600

    Upgrade Ember to 1.13.0 and other misc libraries

--- config/initializers/ember.rb
+++ config/initializers/ember.rb
@@ -1,5 +1,5 @@
 EmberCLI.configure do |c|
-  c.build_timeout = 10
+  c.build_timeout = 20
   c.app :frontend,
         path: Rails.root.join('frontend').to_s,
         exclude_ember_deps: ["jquery"]
